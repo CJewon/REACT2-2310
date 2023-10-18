2b01b5ca613d407e0627f10c76751875e1a1fd32
DDogn929 <112820744+CJewon@users.noreply.github.com>
2023-10-18 14:19:53 +0900

    asd

--- practice/src/ChildContextComp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ChildContextComp() {
-  return (
-    <div>ChildContextComp</div>
-  )
-}
--- practice/src/ContextPractice.js
+++ practice/src/ContextPractice.js
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext } from 'react'
 import ChildContextComp from './ChildContextComp';
 
 const MyContext = createContext();
@@ -7,12 +7,28 @@ export default function ContextPractice() {
     const names = ['철수', '영희', '짱구', '맹구']
   return (
     <MyContext.Provider value={names}>
-        <ChildContextComp></ChildContextComp>
+      <할아버지></할아버지>
     </MyContext.Provider>
   )
 }
 
+function 할아버지() {
+   return <아빠></아빠>
+}
 
+function 아빠() {
+  return <나></나>
+}
+
+function 나() {
+
+  const 이름 = useContext(MyContext);
+  return (
+    <div>
+      {이름}
+    </div>
+   )
+}
 
 
 
