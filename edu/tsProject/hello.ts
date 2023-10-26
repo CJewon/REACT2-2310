d7c3d2c2db7aeb7c7dfbec46a3a368d63e132fe5
DDogn929 <112820744+CJewon@users.noreply.github.com>
2023-10-26 14:33:48 +0900

    wefasdfrqr

--- edu/tsProject/hello.ts
+++ edu/tsProject/hello.ts
@@ -7,9 +7,14 @@ function hello(이름 : string) {
     console.log(`hello! ${이름}`)
 }
 
-function 출생년도계산기(입력나이 : number) {
-    return  2023 - 30
+function 출생년도계산기(입력나이 : number):number {
+    return  2023 - 입력나이;
 }
 
+function 출생년도출력(출생년도 : number) {
+    console.log(출생년도);
+}
+
+출생년도출력(출생년도계산기(나이))
 
 hello("김철수");
