1c74d1122323b96d4f806e6411a831997997a1af
DDogn929 <112820744+CJewon@users.noreply.github.com>
2023-10-26 13:37:36 +0900

    dfawefasdf

---
+++ edu/tsProject/hello.js
@@ -0,0 +1,5 @@
+"use strict";
+function hello(이름) {
+    console.log(`hello! ${이름}`);
+}
+hello("김철수");
---
+++ edu/tsProject/hello.ts
@@ -0,0 +1,15 @@
+let 나이 = 30;
+
+console.log(typeof(나이))
+
+
+function hello(이름 : string) {
+    console.log(`hello! ${이름}`)
+}
+
+function 출생년도계산기(입력나이 : number) {
+    return  2023 - 30
+}
+
+
+hello("김철수");
